d2241e47e07a08cc137555f6da49d0b74be94dd1
Saicharanbanda <146003739+Saicharanbanda@users.noreply.github.com>
2023-10-31 16:23:11 -0400

    Delete Banda-Saicharan_hw3_secondPhaseOfApp/charan.js

--- Banda-Saicharan_hw3_secondPhaseOfApp/charan.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  // Handle incoming requests here
-});
-
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
